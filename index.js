d55a88474bc98d94e6354d77c0b398c0bcc0c4d6
Kalib Riter <klaibriter@gmail.com>
2024-08-29 13:36:39 -0400

    passed tests

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your solution in this file!
+var customerName = 'bob'
+function upperCaseCustomerName() {
+    window.customerName = window.customerName.toUpperCase();
+  }
+function setBestCustomer(bestCustomer) {
+     window.bestCustomer = 'not bob'
+}
+function overwriteBestCustomer(bestCustomer) {
+    window.bestCustomer = 'maybe bob'
+}
+const leastFavoriteCustomer = 'bob'
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'Jerry'
+}
